8125e39ecc251488139e5f0eec32464a6527f505
Avishka Ruwanthi <avishkark999@gmail.com>
2026-01-13 17:40:06 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-      </Routes>
-    </BrowserRouter>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} /> 
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
-import Hero from "../sections/Hero";
-import About from "../sections/About";
-import Projects from "../sections/Projects";
-import Contact from "../sections/Contact";
-import { useActiveSection } from "../hooks/useActiveSection";
+// import Navbar from "../components/layout/Navbar";
+// import Footer from "../components/layout/Footer";
+// import Hero from "../sections/Hero";
+// import About from "../sections/About";
+// import Projects from "../sections/Projects";
+// import Contact from "../sections/Contact";
+// import { useActiveSection } from "../hooks/useActiveSection";
 
 // const Home = () => {
 //   const activeSection = useActiveSection([

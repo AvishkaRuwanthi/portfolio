c4e1dc89cebf27a1ac9ebc74f499467a6b5ad3ec
Avishka Ruwanthi <avishkark999@gmail.com>
2026-01-13 17:09:06 +0530

    Initial React + Vite + Tailwind setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import Navbar from "../components/layout/Navbar";
+import Footer from "../components/layout/Footer";
+import Hero from "../sections/Hero";
+import About from "../sections/About";
+import Projects from "../sections/Projects";
+import Contact from "../sections/Contact";
+import { useActiveSection } from "../hooks/useActiveSection";
+
+// const Home = () => {
+//   const activeSection = useActiveSection([
+//     "home",
+//     "about",
+//     "projects",
+//     "contact",
+//   ]);
+
+//   return (
+//     <div className="relative min-h-screen overflow-x-hidden scroll-smooth">
+//       <Navbar activeSection={activeSection} />
+//       <Hero />
+//       <About />
+//       <Projects />
+//       <Contact />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+export default Home;
